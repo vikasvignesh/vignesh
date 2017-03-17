23481326a434ef3688b37796696c58aa7da64708
vikasvignesh <vikey97v@gmail.com>
2017-03-17 02:22:38 -0700

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.*;
+public class Ew {
+	
+	public static void main(String args[]){
+int a,b,i=0;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+for(i=a;i<=b;i++){
+	if(i%2!=0){
+		System.out.println(i);
+	}
+}
+
+}}
