43cc56cb43c17df2952b92d36a0e48f7a73e1ae2
vikasvignesh <vikey97v@gmail.com>
2017-03-18 17:29:17 -0700

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.*;
+public class Ew {
+	
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+    int i;
+    for(i=0;i<255;i++)
+    {
+    	System.out.println(i+" "+(char)i);
+    }
+}}
