8f9db215b9b163c97274d725f6ae509e9d2483ee
vikasvignesh <vikey97v@gmail.com>
2017-03-25 02:40:14 -0700

    Update working_day.java

--- working_day.java
+++ working_day.java
@@ -12,6 +12,9 @@ String day="sunday";
 if(day.equalsIgnoreCase(working)){
 	System.out.println("Workingday("+working+")-->False");
 }else{
+	if(working.equalsIgnoreCase("monday")||working.equalsIgnoreCase("tuesday")||working.equalsIgnoreCase("wednesday")||working.equalsIgnoreCase("thrusday")||working.equalsIgnoreCase("friday")||working.equalsIgnoreCase("saturday"))
 	System.out.println("Workingday("+working+")-->True");
+	else
+		System.out.println("Invalid day");
 }
 }}
