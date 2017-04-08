ee5876e0942a0f97a9cb5a607bb7653a8e0459d5
vikasvignesh <vikey97v@gmail.com>
2017-04-07 22:43:10 -0700

    Add files via upload

---
+++ pro_least.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char num[10],max=-1,tem;
+int i,j,l=0,del;
+printf("Enter the number\n");
+scanf("%s",num);
+printf("Enter the number element to delete\n");
+scanf("%d",&del);
+l=strlen(num);
+for(i=0;i<l;i++){
+        for(j=i+1;j<l;j++){
+    if(num[i]>num[j]){
+        tem=num[i];
+        num[i]=num[j];
+        num[j]=tem;
+    }}
+}
+i=0;
+while(i<del){
+        num[l-1]=0;
+l--;
+i++;
+}
+printf("The least number after delete %s",num);
+}
