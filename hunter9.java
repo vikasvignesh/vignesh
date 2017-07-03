10bfd2f4a29f4bc4647a5eb0068b793252eb0208
vikasvignesh <vikey97v@gmail.com>
2017-07-03 08:51:04 -0700

    Update hunter9.java

--- hunter9.java
+++ hunter9.java
@@ -1,56 +1,51 @@
-package d;
-import java.util.*;
-public class as {
-	static void findsum(int tem[]){
-		int i,j,n,m,max=-1,min=-999,c=0;
-		for(i=0;i<tem.length;i++){
-			if(tem[i]<0){
-				m=tem[i];
-				n=(tem[i]*-1)+1;
-				for(i=0;i<tem.length;i++)
-				if(n==tem[i]){
-					System.out.println(m+" "+n);
-					break;
-				}else{
-					c++;
-				}
-					if(c==tem.length){
-					for(i=0;i<tem.length;i++){
-						if(tem[i]<0){
-							if(min<tem[i]){
-								min=tem[i];
-							}
-						}else{
-							if(max<tem[i])
-								max=tem[i];
-						}
-					}
-					System.out.println(max+" "+min);
-				}
-				
-			}
+public class Solution {
+static	int[] sumzero(int[] nums){
+	int positivecount=0,negativecount=0;
+	Arrays.sort(nums);
+	int result[]=new int[2];
+	for(int i=0;i<nums.length;i++)
+	{
+		if(nums[i]>0)
+		{
+			result[0]=nums[i];
+			positivecount++;
+			break;
 		}
-		
 	}
-public static void main(String args[]){
-	int n,i,c=0,c1=0;
-	Scanner o=new Scanner(System.in);
-	n=o.nextInt();
-	int arr[]=new int[n];
-	for(i=0;i<n;i++){
-		arr[i]=o.nextInt();
+	for(int i=0;i<nums.length;i++)
+	{
+		if(nums[i]<0)
+		{
+			result[1]=nums[i];
+			negativecount++;
+			break;
+		}
 	}
-	for(i=0;i<n;i++){
-		if(arr[i]>0)
-			c++;
-		else if(arr[i]<0)
-			c1++;
+	if(negativecount==0)
+	{
+	   result[0]=nums[nums.length-1];
+	   result[1]=nums[nums.length-2];
 	}
-	if(c==arr.length)
-		System.out.println("You Entered all positive");
-	else if(c1==arr.length)
-		System.out.println("You Entered all negative");
-	else
-		findsum(arr);
+	else if(positivecount==0)
+	 	return result;
+	 return result;
 	}
-}			
+	
+	
+    public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+    	int n;
+    	System.out.println("Enter size of array");
+    	n=in.nextInt();
+    	int nums[]=new int[n];
+    	System.out.println("Enter the positive and negative number");
+    	for(int i=0;i<n;i++){
+    		nums[i]=in.nextInt();
+    	}
+    	int ans[]=sumzero(nums);
+    	System.out.println("The sum is closer to zero  2");
+    	for(int i=0;i<ans.length;i++){
+    		System.out.println(ans[i]);
+    	}
+    }
+}
